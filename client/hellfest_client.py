b9ab69a7c53de553157af71c9e0a954a21aebfe0
Leo Le Bleis <leo.lebleis@gmail.com>
2021-07-23 13:07:58 +0100

    Adding typings and comments

--- client/hellfest_client.py
+++ client/hellfest_client.py
@@ -1,10 +1,15 @@
 import requests
+from typing import List
 from urllib3.exceptions import HTTPError
 
 URI = 'https://hellfest-hebergement.fr/api/index.php?newAPI=1'
 
 
-def locations_all():
+def locations_all() -> List[dict]:
+    """
+    Fetches
+    :return:
+    """
     body = {'action': 'annonces'}
     r = requests.post(URI, body)
     r.raise_for_status()
