6c084e89c8b467c25f42b5d07d3ca650d0638908
Leo Le Bleis <leo.lebleis@gmail.com>
2021-07-24 15:22:52 +0100

    Getting rid of List typing

--- email_service/formatter.py
+++ email_service/formatter.py
@@ -1,14 +1,12 @@
 from jinja2 import Template
 import os
 
-from typing import List
-
 path_to_here = os.path.dirname(os.path.abspath(__file__))
 f = open(os.path.join(path_to_here, "email.html"))
 html_email = f.read()
 
 
-def render_accommodations(accommodations: List[dict], filters: dict) -> str:
+def render_accommodations(accommodations: list[dict], filters: dict) -> str:
     t = Template(html_email)
     formatted_email = t.render(filters, title="Les locations disponibles...", accommodations=accommodations)
     print(formatted_email)
