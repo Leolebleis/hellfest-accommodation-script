a0ebdd4498473015715e082c91f9a597bd3da1ec
leolebleis <leo.lebleis@gmail.com>
2021-10-03 18:34:01 +0100

    Adding email_client for email notifications

---
+++ client/email_client.py
@@ -0,0 +1,37 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+RECIPIENTS = json.loads(os.getenv("RECIPIENTS"))
+URI = os.getenv("EMAIL_CLIENT_URI")
+
+
+def send_email(html_body: str, subject_line: str) -> None:
+
+    if len(RECIPIENTS) == 0:
+        raise NotEnoughRecipientsError
+
+    uri = URI
+
+    for idx, recipient in enumerate(RECIPIENTS):
+        if idx == 0:
+            uri += f"?recipients={recipient}"
+        else:
+            uri += f"&recipients={recipient}"
+
+    r = requests.post(uri, data=html_body.encode("UTF-8"), headers={
+        "X-API-KEY": API_KEY,
+        "Subject-Line": subject_line
+    })
+
+    r.raise_for_status()
+
+
+class NotEnoughRecipientsError(Exception):
+    ...
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from client import hellfest_client
+from client.email_client import send_email
 from email_service import formatter
 from filters import filter_accommodations
 
@@ -14,4 +15,7 @@ if __name__ == '__main__':
     unfiltered_accommodations = hellfest_client.accommodations_all()
     filtered_accommodations = filter_accommodations(unfiltered_accommodations, filter_values)
     print(filtered_accommodations)
-    formatter.render_accommodations(filtered_accommodations, filter_values)
+    formatted_email = formatter.render_accommodations(filtered_accommodations, filter_values)
+
+    send_email(formatted_email, "Hellfest 2022 - Tous les appartements disponibles :)")
+
